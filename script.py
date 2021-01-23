70808aedd36989386c2079679c4566dd18381e12
Niro-Dexter <77662275+Niro-Dexter@users.noreply.github.com>
2021-01-23 06:36:50 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,22 +3,22 @@ class script(object):
     START_MSG = """ <b>Hi {}
 
 මම තමයි Image Editor Bot.
-බොහෝමයක් photo modes වලට Support කරනවා මම
+බොහෝමයක් Image Editing modes වලට Support කරනවා මම ✅
 
 For more click help....</b>"""
     HELP_MSG = """Hi😊, මෙම පියවර අනුගමනය කරන්න 👇 ..
 
 <code>🌀 ඔයාට Edit කරන්න අවශ්‍ය Image එක මට එවන්න..</code>
 
-<code>🌀 ඔයාට Edit කරන්න අවශ්‍ය ක්‍රමය තෝරගන්න</code>
+<code>🌀 ඔයාට Edit කරන්න අවශ්‍ය ක්‍රමය තෝරගන්න (Editing Mod)</code>
 
-<code>🌀 ඔයා Edit කරපු Image එක මම Upload කරන්නම් 😉 </code>
+<code>🌀 ඔයා Image එක Edit කලාට පස්සෙ මම ඒක ඔයාට Upload කරන්නම් 😉 </code>
 
 
 🔥 @Zitron_Kenway"""
     ABOUT_MSG = """⭕️<b>Developer : ®Zitron Kenway</b>
 
-⭕️<b>Developer :</b> <code>@Zitron_Kenway</code>
+⭕️<b>Channel :</b> <code>@ProjectNetflixLK</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
