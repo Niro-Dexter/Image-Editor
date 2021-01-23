f9ec4d4124f56d11ba1b27e69262ae3e997ce2e2
Niro-Dexter <77662275+Niro-Dexter@users.noreply.github.com>
2021-01-23 06:44:33 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -255,13 +255,13 @@ async def cb_handler(client, query):
     elif query.data == "help_data":
 
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("BACK", callback_data="start_data"), InlineKeyboardButton("ABOUT", callback_data="about_data"), ],
-                                         [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor")]])
+                                         [InlineKeyboardButton("Developer ගැන", url="https://t.me/Zitron__Kenway")]])
 
         await query.message.edit_text(script.HELP_MSG, reply_markup=keyboard, disable_web_page_preview=True)
 
     elif query.data == "about_data":
 
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("BACK", callback_data="help_data"), InlineKeyboardButton("START", callback_data="start_data"), ],
-                                         [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor")]])
+                                         [InlineKeyboardButton("Developer ගැන", url="https://t.me/Zitron_Kenway")]])
 
         await query.message.edit_text(script.ABOUT_MSG, reply_markup=keyboard, disable_web_page_preview=True)
